2570d937ac1a489b3cc5127f1149dc6f6eee0c10
Ivan Shchipin <ivan.shchipin@gmail.com>
2022-11-06 06:07:02 +0600

    hw2.3

--- course02/site/js/script.js
+++ course02/site/js/script.js
@@ -12,6 +12,35 @@ document.addEventListener('mousemove', mouseMove);
 function curVis() {
     cursor.style.display = 'block';
 }
+
 function curNoVis() {
     cursor.style.display = 'none';
-}   
+}
+
+
+// Не смог придумать, как тут можно использовать логические операторы || (логическое ИЛИ) и && (логическое И).*
+const seasons = () => {
+    const winter = [12, 1, 2];
+    const spring = [3, 4, 5];
+    const summer = [6, 7, 8];
+    const autumn = [9, 10, 11];
+
+    function seasonName(month) {
+        if (winter.includes(month)) {
+            return 'Зима';
+        } else if (spring.includes(month)) {
+            return 'Весна';
+        } else if (summer.includes(month)) {
+            return 'Лето';
+        } else if (autumn.includes(month)) {
+            return 'Осень';
+        } else {
+            return 'Введен неправильный номер';
+        }
+    }
+    // Приятнее получать ответ в alert(), нежели в консоль, решил реализовать так :)
+    // но если что, раскомментить не долго %)
+    alert(seasonName(+prompt('Введите номер месяца.')));
+    // console.log(seasonName(+prompt('Введите номер месяца.')));
+
+}
