70f76a5d5da9b656b902007e2f0421c98837c475
Ivan Shchipin <ivan.shchipin@gmail.com>
2022-11-30 23:34:22 +0600

    request refactoring

--- course03/hw15/lib/request.js
+++ course03/hw15/lib/request.js
@@ -1,22 +1,38 @@
 const noop = () => {};
+const NO_PARAMS = {};
+const NO_HEADERS = {};
+const OK_200 = [200];
 
 function request({
    method = 'GET',
    url,
-   type = 'json',
+   params = NO_PARAMS,
+   headers = NO_HEADERS,
+   body,
+   responseType = 'json',
+   requestType = 'json',
+   okResponces = OK_200,
    checkStatusInResponse = false,
    onSuccess = noop,
    onError = noop,
 }) {
    const req = new XMLHttpRequest();
 
-   req.open(method, url);
-   req.responseType = type;
+   const urlParams = new URLSearchParams(params);
+   const queryString = urlParams.toString();
+
+   req.open(method, url + (queryString ? `?${queryString}` : ''));
+
+   Object.keys(headers).forEach((key) => {
+      req.setRequestHeader(key, headers[key]);
+   });
+
+   req.responseType = responseType;
 
    req.onload = function (event) {
       const target = event.target;
 
-      if (target.status !== 200) {
+      if (!okResponces.includes(target.status)) {
          onError(target.statusText);
 
          return;
@@ -35,5 +51,23 @@ function request({
       onError();
    };
 
-   req.send();
+   let dataBody = body;
+
+   if (requestType === 'urlencoded') {
+      req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+      const bodyParams = new URLSearchParams(body);
+
+      dataBody = bodyParams.toString();
+   }
+
+   if (requestType === 'json') {
+      req.setRequestHeader('Content-type', 'application/json');
+
+      const bodyParams = new URLSearchParams(body);
+
+      dataBody = JSON.stringify(body);
+   }
+
+   req.send(dataBody);
 }
